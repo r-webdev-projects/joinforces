837d2beb9a26b84fc2078480718b528e899ec8bd
Vishal Arora <vishal@vishalarora.com>
2015-08-09 13:49:13 -0400

    Pass parent_id param to controller

--- app/controllers/commontator/comments_controller.rb
+++ app/controllers/commontator/comments_controller.rb
@@ -16,7 +16,7 @@ module Commontator
         format.html { redirect_to @thread }
         format.js
       end
-     
+
     end
 
     # POST /threads/1/comments
@@ -24,9 +24,10 @@ module Commontator
       @comment = Comment.new
       @comment.thread = @thread
       @comment.creator = @user
-      @comment.body = params[:comment].nil? ? nil : params[:comment][:body]
+      @comment.body = params[:comment].nil? ? nil : params[:comment].to_s
+
       security_transgression_unless @comment.can_be_created_by?(@user)
-      
+
       respond_to do |format|
         if  !params[:cancel].nil?
           format.html { redirect_to @thread }
@@ -89,7 +90,7 @@ module Commontator
         format.js { render :delete }
       end
     end
-    
+
     # PUT /comments/1/undelete
     def undelete
       security_transgression_unless @comment.can_be_deleted_by?(@user)
@@ -102,11 +103,11 @@ module Commontator
         format.js { render :delete }
       end
     end
-    
+
     # PUT /comments/1/upvote
     def upvote
       security_transgression_unless @comment.can_be_voted_on_by?(@user)
-      
+
       @comment.upvote_from @user
 
       respond_to do |format|
@@ -114,12 +115,12 @@ module Commontator
         format.js { render :vote }
       end
     end
-    
+
     # PUT /comments/1/downvote
     def downvote
       security_transgression_unless @comment.can_be_voted_on_by?(@user) &&\
         @comment.thread.config.comment_voting.to_sym == :ld
-      
+
       @comment.downvote_from @user
 
       respond_to do |format|
@@ -127,11 +128,11 @@ module Commontator
         format.js { render :vote }
       end
     end
-    
+
     # PUT /comments/1/unvote
     def unvote
       security_transgression_unless @comment.can_be_voted_on_by?(@user)
-      
+
       @comment.unvote :voter => @user
 
       respond_to do |format|
@@ -139,9 +140,9 @@ module Commontator
         format.js { render :vote }
       end
     end
-    
+
     protected
-    
+
     def set_comment_and_thread
       @comment = Comment.find(params[:id])
       @thread = @comment.thread
