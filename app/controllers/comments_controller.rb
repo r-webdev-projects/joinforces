f46fee2b0ff3887cd98596e0ca0ada2aff217cba
Vishal Arora <vishal@vishalarora.com>
2015-08-14 10:32:59 -0400

    Force users to authenticate only before posting comments, not viewing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_filter :load_commentable
+  before_filter :authenticate_user!, except: [:index]
 
   def index
     @comments = @commentable.comments
