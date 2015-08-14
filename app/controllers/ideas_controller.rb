8f982596c8b31b690ab6f56f0f8053c07c4a03fe
Vishal Arora <vishal@vishalarora.com>
2015-08-14 10:26:37 -0400

    Force users to sign in/register before posting comments and/or ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,6 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [:show, :edit, :update, :destroy, :kickoff]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /ideas
   def index
