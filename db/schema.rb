b1e096be638b4f05977c960e5581d3339023758c
Vishal Arora <vishal@vishalarora.com>
2015-08-14 15:58:21 -0400

    Add friendly_id to Users, run migration, create routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814165545) do
+ActiveRecord::Schema.define(version: 20150814195517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20150814165545) do
     t.string   "location"
     t.text     "short_bio"
     t.string   "username"
+    t.string   "slug"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
