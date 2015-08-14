83f8041a54a2b0349f5a0c1256a3dd5bb51f94cb
Vishal Arora <vishal@vishalarora.com>
2015-08-14 11:05:28 -0400

    Add ability for comment authors to delete comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,11 @@
 module CommentsHelper
+
+  def comment_by_current_user?(comment)
+    if current_user.id == comment.user.try(:id)
+      return true
+    else
+      return false
+    end
+  end
+
 end
