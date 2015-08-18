5fda03a071de4a1915dcc472d7540c04399e95b5
Vishal Arora <vishal@vishalarora.com>
2015-08-18 17:39:36 -0400

    Configure & implement Markdown parsing with Redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,29 @@
 module ApplicationHelper
 
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      no_styles:       true,
+      safe_links_only: true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+    }
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      highlight:          true,
+      quote:              true,
+      no_intra_emphasis:  true,
+      lax_spacing:        true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
+
 end
