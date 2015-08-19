8f2fd02675e5d0a3851d15b65633d66e40d25210
Vishal Arora <vishal@vishalarora.com>
2015-08-19 14:57:36 -0400

    Add omniauth-related columns to Users, run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814195517) do
+ActiveRecord::Schema.define(version: 20150819185703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,10 +69,14 @@ ActiveRecord::Schema.define(version: 20150814195517) do
     t.text     "short_bio"
     t.string   "username"
     t.string   "slug"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
