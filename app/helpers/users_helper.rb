8d8e2a26e5b400bf43819b67f2e82b55e6619b60
Vishal Arora <vishal@vishalarora.com>
2015-08-22 18:23:43 -0400

    Small style tweaks, change some copy on registration#new view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def total_users(label = '')
+    total_users = User.all.count.to_s
+    if label.present?
+      total_users << " " << label.pluralize(User.all.count)
+    else
+      total_users
+    end
+  end
 end
