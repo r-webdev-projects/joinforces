7bb102d6e59d85cf052b296f0223d740df6b85b6
Josh Stricklin <jostricklin@gmail.com>
2015-08-29 13:57:02 -0500

    added user profile edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+  	@user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For user authentication
   devise_for :users #, :controllers => { registrations: 'registrations' }
-  resources :users, only: [:show]
+  resources :users#, only: [:show]
 
   # Static Pages
   match '/about' => 'static_pages#about', via: 'get'
