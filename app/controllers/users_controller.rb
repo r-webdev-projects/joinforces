fab56785d1fc2557d621bec9b5dd41a02cb04896
Josh Stricklin <jostricklin@gmail.com>
2015-09-24 17:38:57 -0500

    added please log in error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless user_signed_in?
+        flash[:danger] = "Please log in."
         redirect_to login_url
       end
     end
