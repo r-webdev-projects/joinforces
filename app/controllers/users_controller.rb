604f04d6385487315d9944095bbdfa83b16e3932
Josh Stricklin <jostricklin@gmail.com>
2015-09-25 13:09:01 -0500

    added index page to display all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
 
   # only allow currently logged in user to edit/update their own profile
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def edit
