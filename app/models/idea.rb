1d431d91502f003a9240e2aa236450407d09630f
Vishal Arora <vishal@vishalarora.com>
2016-01-01 19:51:20 -0500

    Attempting passing S3 bucket config from model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -9,11 +9,9 @@ class Idea < ActiveRecord::Base
   acts_as_votable
 
    # This method associates the attribute ":image" with a file attachment
-  has_attached_file :image, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
+  has_attached_file :image,
+                    styles: { square: '200x200#', medium: '300x300>'},
+                    bucket: 'joinforces-assets'
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
