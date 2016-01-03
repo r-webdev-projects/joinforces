0c00c314b002056c955513b899fcda184b783a33
Toan <toan@wontoan.com>
2016-01-02 20:29:37 -0500

    Redir to project page on click & add hover effect! =)

--- app/assets/javascripts/static_pages/users.js
+++ app/assets/javascripts/static_pages/users.js
@@ -1,3 +1,9 @@
-(function() {
-  console.log('BANANAS!');
-}());
+$(document).ready(function() {
+
+  //Direct user to specified project page
+  $('.user-project').on('click', function() {
+    var url = $('.user-project-summary').find('a').attr('href');
+    location.href = url;
+  });
+
+});
